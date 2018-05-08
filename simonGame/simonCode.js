23f2b594cf394f87af1034bb6617823d41717cd8
Andrew Bieber <abieber@g.clemson.edu>
2018-05-08 13:18:05 -0400

    Completed the Front End Development Certificate! - Last Project finalized and submitted. All future commits will be improvements, refactoring, or re-organization of the codebase

--- simonGame/simonCode.js
+++ simonGame/simonCode.js
@@ -4,7 +4,6 @@ window.onload = function() {
   var form1 = document.querySelector("form");
   var playerSelect = "";
   var index = 0;
-  //var j = 0;
   var compArr = [];
   var playerArr = [];
 
@@ -15,11 +14,16 @@ window.onload = function() {
       playerSelect = entry[1];
     };
 
+    index = 0;
+    compArr = [];
+    playerArr = [];
+
     var randomInt = Math.floor(Math.random() * Math.floor(4));
     compArr.push(randomInt);
-    console.log(compArr);
-    index = 1;
-    document.getElementById("space5").innerHTML = index;
+    console.log(compArr + " compArr initial value");
+    document.getElementById("space5").innerHTML = compArr.length;
+    document.getElementById("results1").textContent = ("New Game Has Begun! Good Luck!");
+    document.getElementById("results1").className = ("results2");
 
     activateSimon();
 
@@ -30,12 +34,28 @@ window.onload = function() {
 
   function activateSimon() {
 
+    //Establish auto-reset after strict mode mess-up occurs from player
+
+    if (compArr.length == 0) {
+      var randomInt = Math.floor(Math.random() * Math.floor(4));
+      compArr.push(randomInt);
+      console.log(compArr + " compArr initial value");
+      document.getElementById("space5").innerHTML = compArr.length;
+      document.getElementById("results1").textContent = ("New Game Has Begun! (You Have Strict Mode Enabled!)");
+      document.getElementById("results1").className = ("results2");
+    }
+
+    //Begin normal iteration of compArr to activate board
+
     var j = 0;
+
+    //First run through iteration is shown below, this is to prevent a delay on first compArr activation
 
     if (j == 0) {
 
       if (compArr[j] == 0) {
         document.getElementById('space1').className = ('button1a');
+        document.getElementById("sound1").play();
 
         function innerLoop1 () {
           setTimeout(function () {
@@ -47,6 +67,7 @@ window.onload = function() {
 
       } else if (compArr[j] == 1) {
         document.getElementById('space2').className = ('button2a');
+        document.getElementById("sound2").play();
 
         function innerLoop2 () {
           setTimeout(function () {
@@ -58,6 +79,7 @@ window.onload = function() {
 
       } else if (compArr[j] == 2) {
         document.getElementById('space3').className = ('button3a');
+        document.getElementById("sound3").play();
 
         function innerLoop3 () {
           setTimeout(function () {
@@ -69,6 +91,7 @@ window.onload = function() {
 
       } else if (compArr[j] == 3) {
         document.getElementById('space4').className = ('button4a');
+        document.getElementById("sound4").play();
 
         function innerLoop4 () {
           setTimeout(function () {
@@ -89,6 +112,7 @@ window.onload = function() {
 
          if (compArr[j] == 0) {
            document.getElementById('space1').className = ('button1a');
+           document.getElementById("sound1").play();
 
            function innerLoop1 () {
              setTimeout(function () {
@@ -100,6 +124,7 @@ window.onload = function() {
 
          } else if (compArr[j] == 1) {
            document.getElementById('space2').className = ('button2a');
+           document.getElementById("sound2").play();
 
            function innerLoop2 () {
              setTimeout(function () {
@@ -111,6 +136,7 @@ window.onload = function() {
 
          } else if (compArr[j] == 2) {
            document.getElementById('space3').className = ('button3a');
+           document.getElementById("sound3").play();
 
            function innerLoop3 () {
              setTimeout(function () {
@@ -122,6 +148,7 @@ window.onload = function() {
 
          } else if (compArr[j] == 3) {
            document.getElementById('space4').className = ('button4a');
+           document.getElementById("sound4").play();
 
            function innerLoop4 () {
              setTimeout(function () {
@@ -145,8 +172,6 @@ window.onload = function() {
       simonLoop();
     }
 
-
-
   };
 
 
@@ -155,6 +180,10 @@ window.onload = function() {
   function resetData() {
     index = 0;
     document.getElementById("space5").innerHTML = index;
+    compArr = [];
+    playerArr = [];
+    document.getElementById("results1").textContent = ("Game Reset");
+    document.getElementById("results1").className = ("results2");
 
   };
 
@@ -162,67 +191,97 @@ window.onload = function() {
 
   function space1Data() {
 
-    console.log(Math.random());
+    playerArr.push(0);
+    console.log(playerArr + " playerArr");
+
+    document.getElementById("sound1").play();
+
+    checkArr();
 
     //return false;
   };
 
   function space2Data() {
 
-    var spaceVar = document.getElementById("space2").textContent;
+    playerArr.push(1);
+    console.log(playerArr + " playerArr");
 
-    if (playerSelect == "X" && spaceVar == "Open") {
-      document.getElementById("space2").textContent = "X";
-      checkWinner();
-      compMove();
-      checkWinner();
-    } else if (playerSelect == "O" && spaceVar == "Open") {
-      document.getElementById("space2").textContent = "O";
-      checkWinner();
-      compMove();
-      checkWinner();
-    }
+    document.getElementById("sound2").play();
+
+    checkArr();
 
     //return false;
   };
 
   function space3Data() {
 
-    var spaceVar = document.getElementById("space3").textContent;
+    playerArr.push(2);
+    console.log(playerArr + " playerArr");
 
-    if (playerSelect == "X" && spaceVar == "Open") {
-      document.getElementById("space3").textContent = "X";
-      checkWinner();
-      compMove();
-      checkWinner();
-    } else if (playerSelect == "O" && spaceVar == "Open") {
-      document.getElementById("space3").textContent = "O";
-      checkWinner();
-      compMove();
-      checkWinner();
-    }
+    document.getElementById("sound3").play();
+
+    checkArr();
 
     //return false;
   };
 
   function space4Data() {
 
-    var spaceVar = document.getElementById("space4").textContent;
+    playerArr.push(3);
+    console.log(playerArr + " playerArr");
 
-    if (playerSelect == "X" && spaceVar == "Open") {
-      document.getElementById("space4").textContent = "X";
-      checkWinner();
-      compMove();
-      checkWinner();
-    } else if (playerSelect == "O" && spaceVar == "Open") {
-      document.getElementById("space4").textContent = "O";
-      checkWinner();
-      compMove();
-      checkWinner();
-    }
+    document.getElementById("sound4").play();
+
+    checkArr();
 
     //return false;
   };
+
+  //Make function to check each press, and overall winning criteria
+
+  function checkArr() {
+
+    if ((playerArr[index] !== compArr[index]) && playerSelect == "classic") {
+      document.getElementById("results1").textContent = ("WRONG! Try Again");
+      document.getElementById("results1").className = ("results1");
+      index = 0;
+      playerArr = [];
+
+      setTimeout(activateSimon, 2000);
+
+    } else if ((playerArr[index] !== compArr[index]) && playerSelect == "strict") {
+      document.getElementById("results1").textContent = ("WRONG! .. resetting game..");
+      document.getElementById("results1").className = ("results1");
+      index = 0;
+      playerArr = [];
+
+      setTimeout(resetData, 2000);
+      setTimeout(activateSimon, 3000);
+
+    } else {
+      index += 1;
+      console.log(index + " Just added 1 to index");
+      document.getElementById("results1").textContent = ("");
+    }
+
+    if (index == 20) {
+      document.getElementById("results1").textContent = ("You WIN! .. resetting game..");
+      document.getElementById("results1").className = ("results");
+      setTimeout(resetData, 3000);
+    } else if (index == compArr.length) {
+      playerArr = [];
+      index = 0;
+      var randomInt = Math.floor(Math.random() * Math.floor(4));
+      compArr.push(randomInt);
+      document.getElementById('space5').textContent = compArr.length;
+      document.getElementById("results1").textContent = ("Good Work So Far!.. Next Level!");
+      document.getElementById("results1").className = ("results2");
+      console.log(compArr + " compArr, just added a random Int");
+      setTimeout(activateSimon, 2000);
+    }
+
+  }
+
 
   document.getElementById("space1").onclick = space1Data;
   document.getElementById("space2").onclick = space2Data;
